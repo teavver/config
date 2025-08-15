23da2088e10c1a3bb090c36c97bda90062895c1e
teavver <jerzyx11@gmail.com>
2025-08-15 19:13:32 +0200

    format

--- mac/prism/nix/flake.nix
+++ mac/prism/nix/flake.nix
@@ -21,209 +21,215 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{
-    self,
-    nix-darwin,
-    nixpkgs,
-    mac-app-util,
-    nix-homebrew,
-    homebrew-core,
-    homebrew-cask,
-    home-manager,
-    ...
-  }:
-  let
-    configuration = { pkgs, ... }: {
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      mac-app-util,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      home-manager,
+      ...
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
 
-      nix.enable = false;
+          nix.enable = false;
 
-      nixpkgs.config.allowUnfree = true;
+          nixpkgs.config.allowUnfree = true;
 
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [
-        vim
-        tmux
-        curl
-        wget
-        git
-        gh
-        xclip
-        bat
-        fzf
-        coreutils
-        sops
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            vim
+            tmux
+            curl
+            wget
+            git
+            gh
+            xclip
+            bat
+            fzf
+            coreutils
+            sops
 
-        python313
-        uv
-        ruff
+            python313
+            uv
+            ruff
 
-        aerospace
-        stats
-        brave
-        spotify
-        kitty
-        obsidian
-        alt-tab-macos
-        hidden-bar
-        bitwarden-desktop
-				sioyek
-        switchaudio-osx
-      ];
+            aerospace
+            stats
+            brave
+            spotify
+            kitty
+            obsidian
+            alt-tab-macos
+            hidden-bar
+            bitwarden-desktop
+            sioyek
+            switchaudio-osx
+          ];
 
-      # Homebrew apps
-      homebrew = {
+          # Homebrew apps
+          homebrew = {
 
-        enable = true;
-
-        caskArgs.no_quarantine = true;
-        global.autoUpdate = false;
-
-        # Uncomment to install cask packages from Homebrew.
-        casks = [
-          "visual-studio-code"
-          "linearmouse"
-          "chromium"
-          "lulu"
-          "alfred"
-          "vlc"
-          "orbstack"
-          "ollama"
-          "steam"
-        ];
-
-        # Uncomment to install app store apps using mas-cli.
-        # masApps = {
-        #   "Session" = 1521432881;
-        # };
-      };
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
-      # programs.fish.enable = true;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      system.primaryUser = "teaver";
-
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      system.stateVersion = 6;
-
-      # System
-      system.defaults = let
-				systemAppsDir = "/System/Applications";
-				nixAppsDir = "/Applications/Nix Apps";
-			in {
-        LaunchServices = {
-          LSQuarantine = false;
-        };
-
-        dock = {
-          tilesize = 28;
-          autohide  = true;
-          autohide-delay = 0.0;
-          autohide-time-modifier = 0.0;
-          magnification = false;
-          mineffect = "scale";
-          show-recents = false;
-          wvous-br-corner = 1; # Disable hot corner (bottom right)
-
-					persistent-apps = [
-						{ app = "${systemAppsDir}/Mail.app"; }
-						{ app = "${systemAppsDir}/Messages.app"; }
-						{ app = "${nixAppsDir}/Brave Browser.app"; }
-						{ app = "${nixAppsDir}/kitty.app"; }
-						{ app = "/Applications/Visual Studio Code.app"; }
-						{ app = "${nixAppsDir}/Obsidian.app"; }
-						{ app = "${nixAppsDir}/Spotify.app"; }
-						{ app = "${systemAppsDir}/System Settings.app"; }
-					];
-
-        };
-
-				finder = {
-					NewWindowTarget = "Other";
-					NewWindowTargetPath = "/Users/teaver/";
-					_FXShowPosixPathInTitle = true;
-					ShowPathbar = true;
-					AppleShowAllExtensions = true;
-					ShowStatusBar	= true;
-					AppleShowAllFiles = true;
-					FXPreferredViewStyle = "Nlsv"; # List view
-					CreateDesktop = false;
-				};
-
-        loginwindow.GuestEnabled  = false;
-
-        screensaver.askForPassword = true;
-        screensaver.askForPasswordDelay = 0;
-
-        controlcenter.FocusModes = true; # Show in Menu Bar
-
-        WindowManager.GloballyEnabled = false;
-        WindowManager.EnableStandardClickToShowDesktop = false;
-
-        NSGlobalDomain = {
-          NSWindowResizeTime = 0.0;
-          NSWindowShouldDragOnGesture = true;
-
-          KeyRepeat = 2;
-          InitialKeyRepeat = 15;
-
-          AppleShowAllFiles = true;
-          AppleShowAllExtensions = true;
-          ApplePressAndHoldEnabled = false;
-
-          "com.apple.trackpad.scaling" = 3.0;
-          "com.apple.mouse.tapBehavior" = 1;
-          "com.apple.sound.beep.volume" = 0.0;
-          "com.apple.sound.beep.feedback" = 0;
-          "com.apple.swipescrolldirection" = false;
-          "com.apple.trackpad.trackpadCornerClickBehavior" = 1;
-        };
-
-        trackpad = {
-          Clicking = true;
-          ActuationStrength = 0;
-          TrackpadThreeFingerDrag = true;
-        };
-
-      };
-
-      system.keyboard = {
-        enableKeyMapping = true;
-        remapCapsLockToControl = true;
-      };
-    };
-  in
-  {
-    darwinConfigurations."prism" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-
-        home-manager.darwinModules.home-manager
-
-				{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-				}
-
-        mac-app-util.darwinModules.default
-
-        nix-homebrew.darwinModules.nix-homebrew {
-          nix-homebrew = {
-            user = "teaver";
             enable = true;
-            # enableRosetta = true;
-            autoMigrate = true;
+
+            caskArgs.no_quarantine = true;
+            global.autoUpdate = false;
+
+            # Uncomment to install cask packages from Homebrew.
+            casks = [
+              "visual-studio-code"
+              "linearmouse"
+              "chromium"
+              "lulu"
+              "alfred"
+              "vlc"
+              "orbstack"
+              "ollama"
+              "steam"
+            ];
+
+            # Uncomment to install app store apps using mas-cli.
+            # masApps = {
+            #   "Session" = 1521432881;
+            # };
           };
-        }
-      ];
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          programs.zsh.enable = true;
+          # programs.fish.enable = true;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          system.primaryUser = "teaver";
+
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          system.stateVersion = 6;
+
+          # System
+          system.defaults =
+            let
+              systemAppsDir = "/System/Applications";
+              nixAppsDir = "/Applications/Nix Apps";
+            in
+            {
+              LaunchServices = {
+                LSQuarantine = false;
+              };
+
+              dock = {
+                tilesize = 28;
+                autohide = true;
+                autohide-delay = 0.0;
+                autohide-time-modifier = 0.0;
+                magnification = false;
+                mineffect = "scale";
+                show-recents = false;
+                wvous-br-corner = 1; # Disable hot corner (bottom right)
+
+                persistent-apps = [
+                  { app = "${systemAppsDir}/Mail.app"; }
+                  { app = "${systemAppsDir}/Messages.app"; }
+                  { app = "${nixAppsDir}/Brave Browser.app"; }
+                  { app = "${nixAppsDir}/kitty.app"; }
+                  { app = "/Applications/Visual Studio Code.app"; }
+                  { app = "${nixAppsDir}/Obsidian.app"; }
+                  { app = "${nixAppsDir}/Spotify.app"; }
+                  { app = "${systemAppsDir}/System Settings.app"; }
+                ];
+
+              };
+
+              finder = {
+                NewWindowTarget = "Other";
+                NewWindowTargetPath = "/Users/teaver/";
+                _FXShowPosixPathInTitle = true;
+                ShowPathbar = true;
+                AppleShowAllExtensions = true;
+                ShowStatusBar = true;
+                AppleShowAllFiles = true;
+                FXPreferredViewStyle = "Nlsv"; # List view
+                CreateDesktop = false;
+              };
+
+              loginwindow.GuestEnabled = false;
+
+              screensaver.askForPassword = true;
+              screensaver.askForPasswordDelay = 0;
+
+              controlcenter.FocusModes = true; # Show in Menu Bar
+
+              WindowManager.GloballyEnabled = false;
+              WindowManager.EnableStandardClickToShowDesktop = false;
+
+              NSGlobalDomain = {
+                NSWindowResizeTime = 0.0;
+                NSWindowShouldDragOnGesture = true;
+
+                KeyRepeat = 2;
+                InitialKeyRepeat = 15;
+
+                AppleShowAllFiles = true;
+                AppleShowAllExtensions = true;
+                ApplePressAndHoldEnabled = false;
+
+                "com.apple.trackpad.scaling" = 3.0;
+                "com.apple.mouse.tapBehavior" = 1;
+                "com.apple.sound.beep.volume" = 0.0;
+                "com.apple.sound.beep.feedback" = 0;
+                "com.apple.swipescrolldirection" = false;
+                "com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+              };
+
+              trackpad = {
+                Clicking = true;
+                ActuationStrength = 0;
+                TrackpadThreeFingerDrag = true;
+              };
+
+            };
+
+          system.keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToControl = true;
+          };
+        };
+    in
+    {
+      darwinConfigurations."prism" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+
+          home-manager.darwinModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+
+          mac-app-util.darwinModules.default
+
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              user = "teaver";
+              enable = true;
+              # enableRosetta = true;
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
     };
-  };
 }
