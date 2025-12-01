b9f62ec242d4f87e96d2d9ecef6b3c6fbb87bc4c
teavver <jerzyx11@gmail.com>
2025-12-01 13:21:14 +0100

    update

--- voidray/home-manager/dotfiles/nvim.lua
+++ voidray/home-manager/dotfiles/nvim.lua
@@ -1,3 +1,5 @@
+vim.cmd("source ~/.vimrc")
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -16,8 +18,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- (nvim-tree stuff) disable netrw at the very start of your init.lua
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- do not touch
 -- pkgs
@@ -28,10 +30,29 @@ require("lazy").setup({
     {
       'neovim/nvim-lspconfig'
     },
-    -- inline diag
-    -- { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' },
-    -- harpoon
-    { 'ThePrimeagen/harpoon' },
+    -- lazygit
+    {
+      "kdheepak/lazygit.nvim",
+      lazy = true,
+      cmd = {
+          "LazyGit",
+          "LazyGitConfig",
+          "LazyGitCurrentFile",
+          "LazyGitFilter",
+          "LazyGitFilterCurrentFile",
+      },
+      dependencies = {
+          "nvim-lua/plenary.nvim",
+      },
+      keys = {
+          { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      }
+    },
+    -- telescope
+    {
+    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+     dependencies = { 'nvim-lua/plenary.nvim' }
+    },
     -- nvim tree
     {
       "nvim-tree/nvim-tree.lua",
@@ -70,11 +91,6 @@ if not ok then
   vim.cmd 'colorscheme default' -- if the above fails, then use default
 end
 
--- inline diag disable (lsp_lines handles that)
--- vim.diagnostic.config({
---   virtual_text = true,
--- })
-
 -- render whitespace,tab
 vim.opt.listchars = { space = '·', tab = '→ ' }; vim.opt.list = true
 vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#3a3a3a' })
@@ -85,6 +101,30 @@ vim.diagnostic.config({
     current_line = true
   }
 })
+
+-- telescope
+local actions = require("telescope.actions")
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = {
+          actions.move_selection_next, type = "action",
+          opts = { nowait = true, silent = true }
+        },
+        ["<C-k>"] = {
+          actions.move_selection_previous, type = "action",
+          opts = { nowait = true, silent = true }
+        },
+      },
+    },
+  }
+}
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>e', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>E', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- mini stuff setup
 require('mini.ai').setup()
@@ -117,10 +157,19 @@ require('mini.move').setup({
 })
 
 -- >>>> keybinds
--- harpoon
--- ...
--- toggle fe
-vim.keymap.set('n', '<C-t>', ':NvimTreeToggle<CR>', { silent = true })
+-- vimrc fixes
+vim.keymap.set('n', '<leader>Q', ':qa<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q', function()
+  local bufs = vim.fn.getbufinfo({buflisted = 1})
+  if #bufs > 1 then
+    vim.cmd('bp|bd #')
+  else
+    vim.cmd('q')
+  end
+end, { noremap = true, silent = true })
+-- nvim tree
+vim.keymap.set('n', '<C-e>', ':NvimTreeToggle<CR>', { silent = true })
+--
 -- Format with LSP
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format buffer' })
 
@@ -137,19 +186,14 @@ vim.lsp.enable('pyright')              -- py
 vim.lsp.enable('ruff')                 -- py
 vim.lsp.enable('nil_ls')               -- nix
 vim.lsp.enable('marksman')             -- md
-vim.lsp.enable('yaml-language-server') -- yml
 vim.lsp.enable('vtsls')                -- .ts
 vim.lsp.enable('taplo')                -- .toml
 
 -- TODO
--- harpoon docs, setup -> ctrl groups + per-project persistent save
--- fuzzy find (telescope?) on leader+e,
--- leader + g = some trivial git menu ?
--- lsp for JS and zig?
+-- ctrl groups
+-- nvim tree keybinds or other file explorer
 -- read terminal mode, custom per-project executables would be niceeee e.g.
 --      leader+t<1..9> to execute, leader+T<1..9> to set cmd like :uv run main.py
--- breakpoints?
 -- last read about mini.nvim addons cooler surround text objects etc
 --
 
-vim.cmd("source ~/.vimrc")
--- voidray/home-manager/home.nix
+++ voidray/home-manager/home.nix
@@ -28,16 +28,19 @@
   };
 
   home.packages = [
+    pkgs.font-manager
     pkgs.firefox
     pkgs.chromium
     pkgs.obsidian
     pkgs.sioyek
+    pkgs.discord
     pkgs.vlc
     pkgs.pavucontrol
     pkgs.vscode
     pkgs.flatpak
     pkgs.playwright
 
+    pkgs.xev
     pkgs.fuse3
     pkgs.lxappearance
     pkgs.xclip
@@ -65,6 +68,7 @@
     pkgs.bat
     pkgs.just
     pkgs.uv
+    pkgs.lazygit
     pkgs.nodejs-slim_24
     pkgs.pre-commit
     pkgs.act
@@ -194,6 +198,8 @@
     extraConfig = ''
       set-option -g pane-base-index 1
       set-option -g renumber-windows on
+      set-option -g focus-events on
+      set-option -g default-terminal "screen-256color"
       set -g status-position bottom
       set -g status-bg colour234
       set -g status-fg colour66
@@ -204,11 +210,11 @@
       unbind C-b
       unbind C-t
       unbind C-r
+      bind -n C-WheelUpPane run-shell "/bin/sh -c 'kitty @ --to=unix:$(ls -1t /tmp/kitty-main-* | head -n1) set-font-size -- +2'"
+      bind -n C-WheelDownPane run-shell "/bin/sh -c 'kitty @ --to=unix:$(ls -1t /tmp/kitty-main-* | head -n1) set-font-size -- -2'"
       bind-key ` last-window
       bind-key w kill-window
       bind-key e send-prefix
-      bind -n C-WheelUpPane run-shell "/bin/sh -c 'kitty @ --to=unix:$(ls -1t /tmp/kitty-main-* | head -n1) set-font-size -- +2'"
-      bind -n C-WheelDownPane run-shell "/bin/sh -c 'kitty @ --to=unix:$(ls -1t /tmp/kitty-main-* | head -n1) set-font-size -- -2'"
       bind-key l swap-window -t -1\; select-window -t -1
       bind-key r swap-window -t +1\; select-window -t +1
       bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-selection -x
@@ -216,7 +222,6 @@
       bind-key -T copy-mode-vi MouseDragEnd2Pane send-keys -X copy-pipe-and-cancel "xclip -selection clipboard"
       unbind-key -T root MouseDown2Pane
       bind-key -T copy-mode-vi C-c send-keys -X copy-pipe-and-cancel "xclip -selection clipboard"
-
       # start continuum on boot
       set -g @continuum-boot 'on'
     '';
